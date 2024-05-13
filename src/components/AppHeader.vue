b58a0231aac3b80c39057e230859198d2be3c6e7
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-13 17:50:40 +0200

    add-logo-amazon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,11 +6,17 @@ export default{
 </script>
 
 <template>
-    <h1>prova</h1>
-
-    
-
-</template>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <!-- Section: Logo -->
+          <a class="navbar-brand" href="#">
+            <img src="https://logos-world.net/wp-content/uploads/2020/04/Amazon-Logo.png" height="60" alt="Amazon Logo">
+          </a>
+        </div>
+      </nav>
+    </header>
+  </template>
 
 <style scoped lang="scss">
 
