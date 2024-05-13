a76209f053c5e214ff471da598eec0976fc02467
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-13 17:31:19 +0200

    feat: add card

--- src/components/AppItaly.vue
+++ src/components/AppItaly.vue
@@ -1,6 +1,44 @@
 <script>
-export default{
+import ItalyCard from './ItalyCard.vue';
+export default {
     name: 'AppItaly',
+    components: {
+        ItalyCard
+    },
+    data() {
+        return {
+            cards: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "series": "Action Comics",
+                }
+            ]
+        }
+    }
 }
 
 </script>
@@ -11,13 +49,21 @@ export default{
             <h3>Scopri i prodotti Made in Italy</h3>
             <a href="#">Scopri</a>
         </div>
-        
+        <section class="card-list container py-5">
+            <ItalyCard v-for="card in cards" :cardInfo="card"></ItalyCard>
+        </section>
+
     </div>
 
-    
+
 
 </template>
 
 <style scoped lang="scss">
 
+    .card-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
 </style>
---
+++ src/components/ItalyCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'ItalyCard',
+    props: ['cardInfo'],
+}
+</script>
+
+<template>
+    <div class="comic-card">
+        <div class="comic-image">
+            <img :src="cardInfo.thumb">
+        </div>
+
+        <p class="py-2">{{ cardInfo.series }}</p>
+    </div>
+
+</template>
+
+<style>
+    .comic-card {
+        width: calc((100% / 6) - 12px);
+        img {
+            display: block;
+            width: 100%;
+            height: 190px;
+            object-fit: cover;
+            object-position: top;
+        }
+    }
+
+</style>
